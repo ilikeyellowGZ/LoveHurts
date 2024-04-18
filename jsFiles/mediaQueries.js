5cb29168079d10afec92e7960092c3e209d0a8cb
ilikeyellowGZ <thisforthetimewehad@gmail.com>
2024-04-18 23:42:56 +0200

    progress today i just need to wake up  early at 9 tmr so ill just finish tomorrow when i get back

--- jsFiles/mediaQueries.js
+++ jsFiles/mediaQueries.js
@@ -1,23 +1,23 @@
 const mobileNavView = document.getElementById("mobile-nav_view");
+const gameStory = document.getElementById("game-story");
 
 function checkAndResize() {
   const imgArtist = document.querySelector(".artist");
   const imgCupid = document.querySelector(".cupid");
   const imgHannah = document.querySelector(".hannah");
   const imgMusician = document.querySelector(".musician");
-  const gameStoryImgs = document.querySelectorAll(
-    "#game-story .img-container img"
+  const imgFour = document.querySelector("#game-story .img-container .imgFour");
+  const imgThree = document.querySelector(
+    "#game-story .img-container .imgThree"
   );
+  const imgTwo = document.querySelector("#game-story .img-container .imgTwo");
+  const imgOne = document.querySelector("#game-story .img-container .imgOne");
+  const textContainer = document.querySelector(".text-container");
 
   var currentWidth = window.innerWidth;
   var currentHeight = window.innerHeight;
 
-  if (
-    currentHeight <= 720 &&
-    currentHeight >= 660 &&
-    currentWidth >= 350 &&
-    currentWidth <= 500
-  ) {
+  if (currentHeight <= 700 && currentWidth >= 350 && currentWidth <= 500) {
     mobileNavView.style.background = "black";
     imgArtist.style.scale = "0.6";
     imgArtist.style.top = "80%";
@@ -31,11 +31,40 @@ function checkAndResize() {
     imgHannah.style.left = "10%";
     imgMusician.style.scale = "0.5";
     imgMusician.style.top = "1%";
-    gameStoryImgs.style.height = "205px";
+    imgFour.style.width = "215px";
+    imgThree.style.width = "215px";
+    imgTwo.style.width = "215px";
+    imgOne.style.width = "215px";
+    gameStory.style.padding = "0 12px";
+    textContainer.style.scale = "0.85";
+    textContainer.style.alignItems = "center";
+    textContainer.style.padding = "0";
+    textContainer.style.width = "100%";
   } else {
+    mobileNavView.style.background = "";
+    imgArtist.style.scale = "";
+    imgArtist.style.top = "";
+    imgArtist.style.right = "";
+    imgCupid.style.right = "";
+    imgCupid.style.scale = "";
+    imgCupid.style.top = "";
+    imgCupid.style.scale = "";
+    imgHannah.style.scale = "";
+    imgHannah.style.top = "";
+    imgHannah.style.left = "";
+    imgMusician.style.scale = "";
+    imgMusician.style.top = "";
+    imgFour.style.width = "";
+    imgThree.style.width = "";
+    imgTwo.style.width = "";
+    imgOne.style.width = "";
+    gameStory.style.padding = "";
+    textContainer.style.scale = "";
+    textContainer.style.alignItems = "";
+    textContainer.style.padding = "";
+    textContainer.style.width = "";
   }
 }
-
 window.onload = function () {
   checkAndResize();
 };
