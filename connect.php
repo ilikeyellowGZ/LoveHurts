6f5613fda0633805d515670af124b0d920dde04b
ilikeyellowGZ <thisforthetimewehad@gmail.com>
2024-04-28 22:29:00 +0200

    waiting on Goonie boy to the db details so i can log in and see whats happening

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php 
+$firstName = $_POST["fullName"]
+$email = $_POST["email"]
+
+//database connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
